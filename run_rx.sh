6a60c714d9e9a5719daa67bbe18fe94c53d9fe54
Joary Paulo <joarypl@gmail.com>
2018-01-05 17:00:21 -0300

    Generate example run commands

---
+++ run_rx.sh
@@ -0,0 +1,1 @@
+./rx_mimo --args=addr0=192.168.10.2, addr1=192.168.10.3  --rate=25e6  --sync=mimo --channels=0,1 --nsamps=250000000
---
+++ run_tx.sh
@@ -0,0 +1,1 @@
+./tx_mimo --args=addr0=192.168.10.2, addr1=192.168.10.3  --rate=25e6 --freq=100e6 --sync=mimo --channels=0,1 --gain=0
